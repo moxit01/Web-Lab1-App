580ac8292c07ab4839dd7099df3fa164bf1956ac
Moxit <shah.moxit999@gmail.com>
2022-02-21 00:39:11 +0530

    Added output in intro page

--- WEB-Lab-App/Pages/Intro.cshtml.cs
+++ WEB-Lab-App/Pages/Intro.cshtml.cs
@@ -9,8 +9,22 @@ namespace WEB_Lab_App.Pages
 {
     public class IntroModel : PageModel
     {
+        [FromQuery(Name ="Value1")]
+        public int Value1 { get; set; }
+
+
+        [FromQuery(Name = "Value2")]
+        public int Value2 { get; set; }
+
+
+        
+
         public void OnGet()
         {
         }
+
+        
+
+
     }
 }
